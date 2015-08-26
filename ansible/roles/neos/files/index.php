68fda1d107583be4953bf92cea1938aa714bcb11
Michiel Roos <michiel@maxserv.nl>
2015-08-26 20:55:29 +0200

    Update title of neos index site

--- ansible/roles/neos/files/index.php
+++ ansible/roles/neos/files/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>TYPO3 & NEOS box</title>
+	<title>Try Neos</title>
 
 	<style type="text/css">
 		html {
