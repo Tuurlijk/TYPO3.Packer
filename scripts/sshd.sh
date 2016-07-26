23486e400e33c1f5225e007569cbf8eeaab1cd19
Michiel Roos <git@monosock.org>
2016-07-26 12:25:36 +0200

    Update sshd.sh

--- scripts/sshd.sh
+++ scripts/sshd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "KexAlgorithms=diffie-hellman-group1-sha1" >> /etc/ssh/sshd_config
+echo "KexAlgorithms=curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1" >> /etc/ssh/sshd_config
